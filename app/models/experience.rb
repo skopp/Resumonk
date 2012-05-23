69fbffc8d3c0a8dfcde343c6e9d47504e1379604
Cassiano Leal <cassianoleal@gmail.com>
2012-05-23 15:14:13 -0300

    Added position field to experiences for custom ordering

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,4 +2,5 @@ class Experience < ActiveRecord::Base
   belongs_to :resume
   validates :company_name, :designation, :job_description, :start_date, presence: true
 
+  default_scope order('position desc')
 end
