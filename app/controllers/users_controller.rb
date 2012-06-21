5989a90aba86cf37760192ef55cbb383b8d3c97e
Bharani M <bharani91@gmail.com>
2012-06-21 17:48:07 +0530

    sleek template changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :admin_user, only: [:destroy]
   before_filter :signed_in_user, only: [:show]
   before_filter :correct_user, only: [:show]
   
@@ -65,9 +64,4 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         redirect_to root_path unless current_user?(user)
       end
-
-      def admin_user
-        redirect_to root_path unless current_user.admin?
-      end
-      
 end
